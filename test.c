3f99fb60da8c75e359124064d5b40eb57d4e61c5
Sayantan-codes <sayantan.jr15@gmail.com>
2020-10-16 17:51:31 +0530

    final update in test

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 int main()
 {
-  printf("Welcome to our project, an open source. Do contribute");
+  printf("Welcome to our project, an open source. Do contribute.\n");
+  printf("This project is contributed primarily by Gaurav, Sayantan and Shivansh.\n");
+  printf("!--------------------Have a nice day---------------------!\n");
 }
